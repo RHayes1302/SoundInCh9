b297341b14b3ab57656782744c8732dbf3aa041e
RHayes1302 <ramone.hayes@yahoo.com>
2026-07-11 14:57:41 -0700

    End Class1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(lids.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
---
+++ app/src/main/java/com/example/soundinch9/ui/screens/LoginScreen.kt
@@ -0,0 +1,27 @@
+package com.example.soundinch9.ui.screens
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun LoginScreen(){}
+
+
+
+
+
+
+
+
+@Composable
+fun LoginContent(
+    paddingValues: PaddingValues,
+    snackbarHostState: SnackbarHostState,
+    scope: CoroutineScope,
+    onNavigateToRegister: () -> Unit,
+
+
+)
+{}
